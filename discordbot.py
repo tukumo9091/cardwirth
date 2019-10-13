b091a3ba54268157bd57c12ec989c202df35ad24
tukumo9091 <56454820+tukumo9091@users.noreply.github.com>
2019-10-13 18:16:22 +0900

    Create discordbot.py

---
+++ discordbot.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+import os
+import traceback
+
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
